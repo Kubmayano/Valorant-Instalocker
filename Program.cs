52653a65f693c946653ae5574b632f9cb1195dde
Kubmayano <elliot.cornier@outlook.com>
2022-10-31 00:20:12 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+class Instalock
+{
+    class Cursor //Class for moving cursor to set X and Y position
+    {
+        [DllImport("User32.Dll")]
+        public static extern long SetCursorPos(int x, int y);
+    }
+
+    class Vector2 //Class for storing x and y values in single variable
+    {
+        public int x {get; set;}
+        public int y {get; set;}
+
+        public Vector2()
+        {
+            //Empty constructor
+        }
+
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Vector2 cords = new Vector2();
+    }
+}
